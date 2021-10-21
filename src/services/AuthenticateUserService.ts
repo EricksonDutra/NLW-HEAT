6ecfaa9e50747991cbf01f997127a354d76d9563
Erickson Dutra <ericksond10@gmail.com>
2021-10-21 07:18:10 -0400

    User Authentication receving the code

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,20 +1,22 @@
 import axios from "axios"
+import prismaClient from "../prisma"
+import { sign } from "jsonwebtoken"
 
-interface IAcessTokenResponse{
+interface IAcessTokenResponse {
   access_token: string
 }
 
-interface IUserResponse{
+interface IUserResponse {
   avatar_url: string
   login: string
   id: number
   name: string
 }
 
-class AuthenticateUserService{
-  async execute(code: string){
+class AuthenticateUserService {
+  async execute(code: string) {
     const url = "https://github.com/login/oauth/access_token";
-    
+
 
     const { data: accessTokenResponse } = await axios.post<IAcessTokenResponse>(url, null, {
       params: {
@@ -22,21 +24,56 @@ class AuthenticateUserService{
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
       },
-      headers:{
+      headers: {
         "Accept": "application/json"
       }
     });
 
-    const response = await axios.get<IUserResponse>("https://api.github.com/user", { 
+    const response = await axios.get<IUserResponse>("https://api.github.com/user", {
       headers: {
         authorization: `Bearer ${accessTokenResponse.access_token}`
-    },
-  });
+      },
+    }
+    );
 
+    const { login, id, avatar_url, name } = response.data
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    })
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      })
+    }
+
+    const token = sign({
+      user: {
+        name: user.name,
+        avatar_url: user.avatar_url,
+        id: user.id
+      }
+    },
+      process.env.JWT_SECRET,
+     {
+        subject: user.id,
+        expiresIn: "1d"
+      }
+    );
+
+    return { token, user }
 
     return response.data;
   }
-  
+
 }
 
 export { AuthenticateUserService };
